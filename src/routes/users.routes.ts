7fbcddcc2ab38f23e341f9f53f1bd91f5eca93f4
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-06-15 20:49:44 -0300

    Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import UpdateUserAvatarService from "../service/UpdateUserAvatarService";
 
 import CreateUserService from "../service/CreateUserService";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -21,5 +26,23 @@ usersRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.file.fieldname,
+        avatarFilename: request.file.fieldname,
+      });
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
