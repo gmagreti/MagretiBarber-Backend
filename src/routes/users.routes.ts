e5e72ce6388f0ccb377f0fe71da3596f5210fee1
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-06-15 21:32:42 -0300

    Tratativa de erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -32,16 +32,12 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single("avatar"),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.file.fieldname,
-        avatarFilename: request.file.fieldname,
-      });
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.file.fieldname,
+      avatarFilename: request.file.fieldname,
+    });
+    return response.json(user);
   }
 );
 
